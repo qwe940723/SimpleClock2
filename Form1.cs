1c88b5e664ad5527fa7a8bcc75b715ccdbd6658d
qwe940723 <qwe940723@gmail.com>
2025-06-21 18:29:34 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,25 @@ namespace SimpleClock2
 
             timerClcok.Start();      
         }
+        List<string> hours = new List<string>();           
+        List<string> minutes = new List<string>();          
+
+        string strSelectTime = "";                         
+
+        private WaveOutEvent waveOut;                       
+        private AudioFileReader audioFileReader;
+        private void comboboxInitialzation()
+        {
+           
+            for (int i = 0; i <= 23; i++)
+                cmbHour.Items.Add(string.Format("{0:00}", i));
+            cmbHour.SelectedIndex = 0;
+
+           
+            for (int i = 0; i <= 59; i++)
+                cmbMin.Items.Add(string.Format("{0:00}", i));
+            cmbMin.SelectedIndex = 0;
+        }
 
         private void timerClcok_Tick(object sender, EventArgs e)
         {
